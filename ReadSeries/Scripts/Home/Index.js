933c149e256d576b8ca8fa70031ecd927eb813aa
Sheila C.S Arnold <sheila.arnoldarca@gmail.com>
2022-02-12 18:58:31 -0300

    Correção: UseStaticFiles
    Correção na inclusão de arquivos staticos presentes nas pastas wwwroot e Scripts

--- ReadSeries/Scripts/Home/Index.js
+++ ReadSeries/Scripts/Home/Index.js
@@ -1,3 +1,30 @@
 ﻿$(document).ready(function () {
-    alert("Hello, World.");
+    console.log("Machu Picchu")
+
+    $("#btnLer").click(function () {
+        console.log("aqui")
+        var formData = new FormData(this);
+
+        $.ajax({
+            url: window.location.pathname,
+            type: 'POST',
+            data: formData,
+            success: function (data) {
+                alert("sucesso")
+                alert(data)
+            },
+            cache: false,
+            contentType: false,
+            processData: false,
+            xhr: function () { // Custom XMLHttpRequest
+                var myXhr = $.ajaxSettings.xhr();
+                if (myXhr.upload) { // Avalia se tem suporte a propriedade upload
+                    myXhr.upload.addEventListener('progress', function () {
+                        /* faz alguma coisa durante o progresso do upload */
+                    }, false);
+                }
+                return myXhr;
+            }
+        });
+    });
 });
--- ReadSeries/Startup.cs
+++ ReadSeries/Startup.cs
@@ -41,7 +41,10 @@ namespace ReadSeries
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+            
             app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
             app.UseStaticFiles(new StaticFileOptions
             {
                 FileProvider = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Scripts")),

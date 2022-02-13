d22ead5f599f34bd60ee631edc7728fe30e9b7e2
Sheila C.S Arnold <sheila.arnoldarca@gmail.com>
2022-02-13 06:31:41 -0300

    Tabela de valores máximos e mínimos
    Inclusão de tabela para impressão de maiores e menores valores para cada classificação/linha do gráfico.  Cálculo dos maiores e menores valores de cada classificação/linha do gráfico.

--- ReadSeries/Scripts/Home/Index.js
+++ ReadSeries/Scripts/Home/Index.js
@@ -1,6 +1,8 @@
 ﻿
 function Enviar() {
 
+    $("#resumo").addClass('d-none');//oculta tabela de apresentação de valores máximos e mínimos
+
     var arquivo = $('#arquivo').prop("files");
 
     if (arquivo[0].type == "application/vnd.ms-excel") {
@@ -30,18 +32,34 @@ function Enviar() {
 
 }
 
-function exibeGrafico(data) {
+function exibeGrafico(data) {   
     var datas = [];
     var data_displacement50ZI001 = [];
     var data_displacement50ZI002 = [];
     var data_displacement50ZI003 = [];
+
     var series_grafico = [];
+
+    var max_displacement50ZI001 = -1000;
+    var max_displacement50ZI002 = -1000;
+    var max_displacement50ZI003 = -1000;
+    var min_displacement50ZI001 = 100000;
+    var min_displacement50ZI002 = 100000;
+    var min_displacement50ZI003 = 100000;
 
     Object.keys(data).forEach(function (item) {
         datas.push(data[item].Data);
         data_displacement50ZI001.push(data[item]["50ZI001/COTA-DIF - Displacement (mm)"] != "" ? parseFloat((data[item]["50ZI001/COTA-DIF - Displacement (mm)"]).replace(',', '.')) : 0);
         data_displacement50ZI002.push(data[item]["50ZI002/COTA-DIF - Displacement (mm)"] != "" ? parseFloat((data[item]["50ZI002/COTA-DIF - Displacement (mm)"]).replace(',', '.')) : 0);
         data_displacement50ZI003.push(data[item]["50ZI003/COTA-DIF - Displacement (mm)"] != "" ? parseFloat((data[item]["50ZI003/COTA-DIF - Displacement (mm)"]).replace(',', '.')) : 0);
+
+        max_displacement50ZI001 = data[item]["50ZI001/COTA-DIF - Displacement (mm)"] != "" ? parseFloat((data[item]["50ZI001/COTA-DIF - Displacement (mm)"]).replace(',', '.')) > max_displacement50ZI001 ? parseFloat((data[item]["50ZI001/COTA-DIF - Displacement (mm)"]).replace(',', '.')) : max_displacement50ZI001 : 0 > max_displacement50ZI001 ? 0 : max_displacement50ZI001 ;
+        max_displacement50ZI002 = data[item]["50ZI002/COTA-DIF - Displacement (mm)"] != "" ? parseFloat((data[item]["50ZI002/COTA-DIF - Displacement (mm)"]).replace(',', '.')) > max_displacement50ZI002 ? parseFloat((data[item]["50ZI002/COTA-DIF - Displacement (mm)"]).replace(',', '.')) : max_displacement50ZI002 : 0 > max_displacement50ZI001 ? 0 : max_displacement50ZI002;
+        max_displacement50ZI003 = data[item]["50ZI003/COTA-DIF - Displacement (mm)"] != "" ? parseFloat((data[item]["50ZI003/COTA-DIF - Displacement (mm)"]).replace(',', '.')) > max_displacement50ZI003 ? parseFloat((data[item]["50ZI003/COTA-DIF - Displacement (mm)"]).replace(',', '.')) : max_displacement50ZI003 : 0 > max_displacement50ZI003 ? 0 : max_displacement50ZI003;
+
+        min_displacement50ZI001 = data[item]["50ZI001/COTA-DIF - Displacement (mm)"] != "" ? parseFloat((data[item]["50ZI001/COTA-DIF - Displacement (mm)"]).replace(',', '.')) < min_displacement50ZI001 ? parseFloat((data[item]["50ZI001/COTA-DIF - Displacement (mm)"]).replace(',', '.')) : min_displacement50ZI001 : 0 < min_displacement50ZI001 ? 0 : min_displacement50ZI001;
+        min_displacement50ZI002 = data[item]["50ZI002/COTA-DIF - Displacement (mm)"] != "" ? parseFloat((data[item]["50ZI002/COTA-DIF - Displacement (mm)"]).replace(',', '.')) < min_displacement50ZI002 ? parseFloat((data[item]["50ZI002/COTA-DIF - Displacement (mm)"]).replace(',', '.')) : min_displacement50ZI002 : 0 < min_displacement50ZI002 ? 0 : min_displacement50ZI002;
+        min_displacement50ZI003 = data[item]["50ZI003/COTA-DIF - Displacement (mm)"] != "" ? parseFloat((data[item]["50ZI003/COTA-DIF - Displacement (mm)"]).replace(',', '.')) < min_displacement50ZI003 ? parseFloat((data[item]["50ZI003/COTA-DIF - Displacement (mm)"]).replace(',', '.')) : min_displacement50ZI003 : 0 < min_displacement50ZI003 ? 0 : min_displacement50ZI003;
     });
 
     //Verifica se o gráfico já foi "montado"
@@ -85,4 +103,13 @@ function exibeGrafico(data) {
     };
 
     myChart.setOption(option);
+
+    $("#resumo").removeClass('d-none');//exibe tabela de apresentação de valores máximos e mínimos
+    document.getElementById("cota1max").innerHTML = max_displacement50ZI001;
+    document.getElementById("cota2max").innerHTML = max_displacement50ZI002;
+    document.getElementById("cota3max").innerHTML = max_displacement50ZI003;
+    document.getElementById("cota1min").innerHTML = min_displacement50ZI001;
+    document.getElementById("cota2min").innerHTML = min_displacement50ZI002;
+    document.getElementById("cota3min").innerHTML = min_displacement50ZI003;
+
 }
